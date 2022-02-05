b6e198f0c4d89788068ac1e1e6db850b245293a3
Rmachado98 <rafaelriibeiro@gmail.com>
2022-02-05 01:07:55 -0300

    Criação do código inicial

---
+++ PesoDaPeca.js
@@ -0,0 +1,24 @@
+//Atividade Remota 1 - UC8
+
+var pesoPeca = 10;
+ if (pesoPeca >= 100) {
+     console.log("Cadastro Permitido") }
+else {console.log("Peso insuficiente, só podem ser cadastradas peças acima de 100g")}
+ 
+
+ var quantidadeDePeca = 10
+
+ if (quantidadeDePeca < 10 ) {
+    console.log("Cadastro Permitido")}
+else {console.log("Capacidade insuficiente na caixa, cadastro não permitido")}
+
+var nomeDaPeca = "Fechadura";
+
+console.log ("O nome da peça possui:", nomeDaPeca.length, "caracteres")
+
+if (nomeDaPeca < 3) {
+    console.log("Nome da peça insuficiente, o nome da peça deve ter mais de 3 caracteres")  
+} 
+
+else 
+{console.log("Peça cadastrada")}
